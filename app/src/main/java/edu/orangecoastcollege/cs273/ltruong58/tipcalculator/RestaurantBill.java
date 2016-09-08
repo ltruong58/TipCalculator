95083cc1f1b4cbfd10f7b22ee924ca67f5ad8c3c
Truong <ltruong58@student.cccd.edu>
2016-09-08 16:34:57 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/ltruong58/tipcalculator/RestaurantBill.java
@@ -0,0 +1,58 @@
+package edu.orangecoastcollege.cs273.ltruong58.tipcalculator;
+
+/**
+ * Process information for restaurant bill.
+ * @author ltruong58
+ * Created by ltruong58 on 9/8/2016.
+ */
+public class RestaurantBill {
+    private double mAmount;
+    private double mTipPercent;
+    private double mTipAmount;
+    private double mTotalAmount;
+
+
+    public RestaurantBill() {
+        mAmount = 0.0;
+        mTipPercent = 0.0;
+        mTipAmount = 0.0;
+        mTotalAmount = 0.0;
+    }
+    public RestaurantBill(double mAmount, double mTipPercent) {
+        this.mAmount = mAmount;
+        this.mTipPercent = mTipPercent;
+        recalculateAmount();
+    }
+
+    public double getAmount() {
+        return mAmount;
+    }
+
+    public double getTipPercent() {
+        return mTipPercent;
+    }
+
+    public double getTipAmount() {
+        return mTipAmount;
+    }
+
+    public double getTotalAmount() {
+        return mTotalAmount;
+    }
+
+    public void setAmount(double mAmount) {
+        this.mAmount = mAmount;
+        recalculateAmount();
+    }
+
+    public void setTipPercent(double mTipPercent) {
+        this.mTipPercent = mTipPercent;
+        recalculateAmount();
+    }
+
+    private void recalculateAmount()
+    {
+        mTipAmount = mAmount * mTipPercent;
+        mTotalAmount = mAmount + mTipAmount;
+    }
+}
